00d9a165f48ffce90ddcdc67d15517243a94d98e
arvind9140 <ar789maurya@gmail.com>
2022-08-24 23:02:06 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,69 @@
-import "./App.css"
-import Header from "./components/common/header/Header"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import About from "./components/about/About"
-import CourseHome from "./components/allcourses/CourseHome"
-import Team from "./components/team/Team"
-import Pricing from "./components/pricing/Pricing"
-import Blog from "./components/blog/Blog"
-import Contact from "./components/contact/Contact"
-import Footer from "./components/common/footer/Footer"
-import Home from "./components/home/Home"
-function App() {
-  return (
-    <>
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/courses' component={CourseHome} />
-          <Route exact path='/team' component={Team} />
-          <Route exact path='/pricing' component={Pricing} />
-          <Route exact path='/journal' component={Blog} />
-          <Route exact path='/contact' component={Contact} />
-        </Switch>
-        <Footer />
-      </Router>
-    </>
-  )
-}
+import React from 'react';
+import IpInfo from './IpInfo';
 
-export default App
+class App extends React.Component{
+  state={
+    ipInput:'',
+    domainInput:'',
+    location:{},
+    ip:'',
+    
+  }
+  
+  componentDidMount(){
+    this.fetchData();
+  }
+  
+  
+
+  
+
+  
+  fetchData=()=>{
+    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_fiWZKh3O2XTXdAwr3zKclm8K5mH6M&ipAddress=${this.state.ipInput}&domain=${this.state.domainInput}`)
+    .then((response)=>{
+      if (response.status === 200){
+    return response.json()
+    .then((data)=>{
+      console.log(data);
+      this.setState({location:data.location, ip:data.ip });
+     
+    })}
+      else {
+        throw new Error('bad response');
+      }
+
+  })
+    .catch(()=>alert('Press OK then get details'))
+    
+        
+  }
+
+  changeIp =(event)=>{
+    this.setState({ipInput:event.target.value, domainInput:event.target.value})
+  }
+  getIpInfo =(event)=> {
+    event.preventDefault();
+    this.fetchData();
+  }
+  render(){
+    return(
+      <div className="app-container">
+        <div className="header">
+          <h1>IP Address Display</h1>
+          <form onSubmit={this.getIpInfo}>
+            <input type='text' onChange={this.changeIp} name="input" value={this.state.ipInput} placeholder="Search for any IP address or domain"></input>
+          
+          </form>
+          
+        </div>
+        <IpInfo location={this.state.location} ip={this.state.ip} />
+        <div id="mapid">
+          
+        </div>
+      </div>
+    )
+  }
+
+}
+export default App;
