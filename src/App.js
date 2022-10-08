ef18d918d1cf72a3eecc2767690f714c8b4b8441
arvind9140 <ar789maurya@gmail.com>
2022-10-08 16:18:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from './Header';
-import Content from './Content';
-import AddressDisplay from './AdressDisplay';
-import WsConnector from './WsConnector';
 
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Header headerText="Sextant" />
-                <Content name="Public IPv4 Address">
-                    <AddressDisplay url='https://api.ipify.org?format=json' />
-                </Content>
-                <Content name="Public IPv6 Address">
-                    <AddressDisplay url='https://api64.ipify.org?format=json' />
-                </Content>
-                <Content name="WebSocket Packet Latency">
-                    <WsConnector />
-                </Content>
-            </div>
-        );
-    }
+import "./App.css"
+import Header from "./components/common/header/Header"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import About from "./components/about/About"
+import CourseHome from "./components/allcourses/CourseHome"
+import Team from "./components/team/Team"
+import Pricing from "./components/pricing/Pricing"
+import Blog from "./components/blog/Blog"
+import Contact from "./components/contact/Contact"
+import Footer from "./components/common/footer/Footer"
+import Home from "./components/home/Home"
+
+function App() {
+ 
+
+  return (
+   
+    <>
+    
+    
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/courses' component={CourseHome} />
+          <Route exact path='/team' component={Team} />
+          <Route exact path='/pricing' component={Pricing} />
+          <Route exact path='/journal' component={Blog} />
+          <Route exact path='/contact' component={Contact} />
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  )
 }
 
 export default App;
